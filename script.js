42b0161c63f5740a949b574cfc74eb3fd4eed3ba
CrysParteka <162991031+CrysParteka@users.noreply.github.com>
2024-07-01 10:57:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -45,11 +45,18 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-ffunction mostraPergunta() {
+function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
 
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
 
 mostraPergunta();
-
